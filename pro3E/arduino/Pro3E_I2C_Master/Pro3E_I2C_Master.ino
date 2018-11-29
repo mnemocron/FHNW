d01b088b7a378f08e1486fc9458a100c615730eb
Simon M. Burkhardt <simon.burkhardt.prime@gmail.com>
2018-11-29 12:38:55 +0100

    added Pro3E Arduino Sketch

---
+++ pro3E/arduino/Pro3E_I2C_Master/Pro3E_I2C_Master.ino
@@ -0,0 +1,78 @@
+#include <Wire.h>
+
+#define BYTE_STATUS 0x11
+#define BYTE_VOLT   0x22
+#define BYTE_TEMP   0x33
+
+// nur für Testzwecke
+#define WIRE_SLAVE_ADDRESS 0x70
+
+// globale Variablen um den Status eines aktuellen Slave BMS' abzuspeichern
+// volatile weil diese von Wire Interrupts verändert werden
+volatile float slave_voltage = 4.0f;
+volatile uint8_t slave_status = 0;
+volatile uint8_t slave_voltage_bytes[2] = {0,0};
+volatile uint8_t slave_temperature_bytes[5] = {0,0,0,0,0};
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  Serial.println("requesting status");
+  requestFromSlave(WIRE_SLAVE_ADDRESS, BYTE_STATUS, 1);
+  Serial.println("requesting voltage");
+  requestFromSlave(WIRE_SLAVE_ADDRESS, BYTE_VOLT, 2);
+  Serial.println("requesting temperature");
+  requestFromSlave(WIRE_SLAVE_ADDRESS, BYTE_TEMP, 5);
+
+  slave_voltage = calculateSlaveVoltage(slave_voltage_bytes[0], slave_voltage_bytes[1]);
+  Serial.print("Slave status: ");
+  Serial.println((char)slave_status);
+  Serial.print("Voltage: ");
+  Serial.println(slave_voltage);
+  Serial.print("Temperature: ");
+  for(int i=0; i<5; i++){
+    Serial.print((char)slave_temperature_bytes[i]);
+  } Serial.println();
+  
+  delay(3000);
+}
+
+/**
+ * @Todo korrekte Konvention für Spannungs werte -> Dennis
+ */
+float calculateSlaveVoltage(uint8_t hibyte, uint8_t lobyte){
+  return (float)hibyte + ((float)lobyte)/10.0f;
+}
+
+/**
+ * Funktion um mehrere Datenbytes von den Slave BMS Boards zu holen
+ */
+void requestFromSlave(uint8_t addr, uint8_t reg, uint8_t len){
+  Wire.beginTransmission(addr);   // start
+  Wire.write(reg);                // write the request command
+  Wire.endTransmission(false);    // restart
+  Wire.requestFrom(addr, len);    // request the bytes
+  uint8_t i = 0;
+  while (Wire.available()) {      // read all bytes
+    char c = Wire.read();
+    if(reg == BYTE_STATUS)
+      slave_status = (uint8_t)c;
+    if(reg == BYTE_VOLT)
+      slave_voltage_bytes[i%2] = (uint8_t)c;
+    if(reg == BYTE_TEMP)
+      slave_temperature_bytes[i%5] = (uint8_t)c;
+    i++;
+  }
+  Wire.endTransmission();         // stop
+}
+
+
+
+
+
---
+++ pro3E/arduino/Pro3E_I2C_Slave/Pro3E_I2C_Slave.ino
@@ -0,0 +1,80 @@
+#include <Wire.h>
+
+#define BYTE_STATUS 0x11
+#define BYTE_VOLT   0x22
+#define BYTE_TEMP   0x33
+
+// nur für Testzwecke
+#define WIRE_OWN_ADDRESS 0x70
+
+// um den Si8600 einzuschalten
+#define PIN_ENABLE_WIRE A3
+
+volatile uint8_t wire_action = 0;
+
+void setup() {
+  Wire.begin(WIRE_OWN_ADDRESS); // join i2c bus with own address
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);           // start serial for output
+  pinMode(PIN_ENABLE_WIRE, OUTPUT);
+  digitalWrite(PIN_ENABLE_WIRE, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+}
+
+void receiveEvent(int howMany) {
+  uint8_t val = Wire.read();    // receive byte as an integer
+  Serial.print(val);
+  switch(val){
+    case BYTE_STATUS:
+      Serial.println(": request status");
+      wire_action = BYTE_STATUS;
+      break;
+    case BYTE_VOLT:
+      Serial.println(": request voltage");
+      wire_action = BYTE_VOLT;
+      break;
+    case BYTE_TEMP:
+      Serial.println(": request temperature");
+      wire_action = BYTE_TEMP;
+      break;
+    default:
+      // @Todo: hier weitere commands (nicht read request)
+      // Turn balancing on/off
+      break;
+  }
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  switch(wire_action){
+    case BYTE_STATUS:
+      Serial.println("send status y");
+      Wire.write("y");  // = ok
+      break;
+    case BYTE_VOLT:
+      Serial.println("send voltage 42");
+      Wire.write(random(2,4)); // generate radnom data for testing the master
+      Wire.write(random(0,9)); // @Todo: Konvention für Spannungsbytes -> Dennis
+      break;
+    case BYTE_TEMP:
+      Serial.println("send temperature");
+      Wire.write("T="); // generate radnom data for testing the master
+      Wire.write(random(255));
+      Wire.write(random(255));
+      Wire.write(random(255));
+      break;
+    default:
+      break;
+  }
+  wire_action = 0;
+  Serial.print(wire_action);
+  Serial.println(" = reset");
+}
+
+
